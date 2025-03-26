2fc86d38e3c66b47f7cab52a9a8d0ed3707356ad
jiu <jiu@jiuui-MacBookPro.local>
2025-03-26 14:17:23 +0900

    style: EndingButton 스타일 구현

---
+++ src/components/header/EndingButton.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import SearchSVG from '../../assets/images/search.svg';
+import {scale} from 'react-native-size-matters';
+import styled from 'styled-components/native';
+import CustomText from '../../utils/CustomText';
+const EndingButton = (): React.JSX.Element => {
+  return (
+    <EndingButtonContainer>
+      <CustomText font="NPSfont_regular">엔딩 만들기</CustomText>
+    </EndingButtonContainer>
+  );
+};
+const EndingButtonContainer = styled.TouchableOpacity`
+  position: absolute;
+  right: ${scale(10)}px;
+  width: ${scale(40)}px;
+  height: ${scale(20)}px;
+  justify-content: center;
+  align-items: center;
+  background-color: #fff;
+  border-radius: ${scale(15)}px;
+`;
+export default EndingButton;
