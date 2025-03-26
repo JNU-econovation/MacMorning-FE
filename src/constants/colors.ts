b07e6c88fd1a68da82e038fe6a6d6bd9770ac129
jiu <jiu@jiuui-MacBookPro.local>
2025-03-26 11:16:32 +0900

    chore: 메인 컬러 상수 작성

---
+++ src/constants/colors.ts
@@ -0,0 +1,11 @@
+export const colors = {
+  primary: '#2897e5',
+  secondary: '#C2E6FF',
+  tertiary: '#E4E4E4',
+  background: '#ffffff',
+  text: {
+    primary: '#000000',
+    secondary: '#AEAEAE',
+    white: '#ffffff',
+  },
+} as const;
