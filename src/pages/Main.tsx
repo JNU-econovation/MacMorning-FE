297219ddeb01c692cccae3457794d90ce03ea325
jiu <jiu@jiuui-MacBookPro.local>
2025-03-26 11:20:30 +0900

    feat: 상단바 겹침 문제 해결

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,15 +1,31 @@
 import React from 'react';
 import styled from 'styled-components/native';
-import {Text, View, useWindowDimensions} from 'react-native';
+import {
+  SafeAreaView,
+  Text,
+  View,
+  useWindowDimensions,
+  ScrollView,
+} from 'react-native';
 import Header from '../components/header/Header';
-import {SafeAreaView} from 'react-native';
+import {colors} from '../constants/colors';
 
 function Main(): React.JSX.Element {
   return (
-    <SafeAreaView className="bg-white">
+    <MainContainer className="bg-white">
       <Header title="이야기 도서관" headerType="default" />
-    </SafeAreaView>
+      <ScrollView>
+        <View className="bg-red-500">
+          <Text>Hello</Text>
+        </View>
+      </ScrollView>
+    </MainContainer>
   );
 }
+
+const MainContainer = styled.View`
+  flex: 1;
+  background-color: #ffffff;
+`;
 
 export default Main;
