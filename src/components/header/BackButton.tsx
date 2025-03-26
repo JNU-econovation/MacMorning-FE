5a2cbbe807a528bbe506cc68524bc2ee8db5e1e3
jiu <jiu@jiuui-MacBookPro.local>
2025-03-26 11:31:44 +0900

    feat: BackButton 버튼으로 변경

--- src/components/header/BackButton.tsx
+++ src/components/header/BackButton.tsx
@@ -1,8 +1,22 @@
 import React from 'react';
 import ArrowLeftSVG from '../../assets/images/arrow-left.svg';
 import {scale} from 'react-native-size-matters';
-const BackButton = (): React.JSX.Element => {
-  return <ArrowLeftSVG color="#fff" width={scale(15)} height={scale(15)} />;
-};
+import styled from 'styled-components/native';
+import {TouchableOpacity} from 'react-native';
 
+const BackButton = (): React.JSX.Element => {
+  return (
+    <BackbuttonContainer>
+      <ArrowLeftSVG color="#fff" width={scale(15)} height={scale(15)} />
+    </BackbuttonContainer>
+  );
+};
+const BackbuttonContainer = styled.TouchableOpacity`
+  position: absolute;
+  left: ${scale(10)}px;
+  width: ${scale(30)}px;
+  height: 100%;
+  justify-content: center;
+  align-items: center;
+`;
 export default BackButton;
