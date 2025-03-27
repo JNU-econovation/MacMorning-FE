be0b8ccf5f6cf35fa9f5c6f63a76b3ddeb68af9a
jiu <jiu@jiuui-MacBookPro.local>
2025-03-27 16:42:44 +0900

    style: BottomBar button 스타일 구현

---
+++ src/components/common/bottomBar/BottomBarButton.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import styled from 'styled-components/native';
+import {scale} from 'react-native-size-matters';
+import {colors} from '../../../constants/colors';
+import {Text} from 'react-native';
+import CustomText from '../../../utils/CustomText';
+
+interface BottomBarButtonProps {
+  icon: React.ReactNode;
+  buttonName: string;
+  onPress: () => void;
+}
+
+const BottomBarButton = ({
+  icon,
+  buttonName,
+  onPress,
+}: BottomBarButtonProps): React.JSX.Element => {
+  return (
+    <BottomBarButtonContainer onPress={onPress}>
+      <Text
+        style={{
+          color: colors.text.primary,
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}>
+        {icon}
+      </Text>
+      <CustomText font="NPSfont_regular" style={{fontSize: scale(7)}}>
+        {buttonName}
+      </CustomText>
+    </BottomBarButtonContainer>
+  );
+};
+
+const BottomBarButtonContainer = styled.TouchableOpacity`
+  justify-content: center;
+  align-items: center;
+  background-color: ${colors.background.lightGray};
+  gap: ${scale(2)}px;
+`;
+
+export default BottomBarButton;
