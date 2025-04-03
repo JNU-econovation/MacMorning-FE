c5f00e5bda99e35ae964f6ce05e284a1f6e20b4d
jiu <jiu@jiuui-MacBookPro.local>
2025-04-03 16:41:07 +0900

    data: 임시 데이터 생성

---
+++ src/apis/book/mock.ts
@@ -0,0 +1,32 @@
+const mockBooks = [
+  {
+    id: '1',
+    title: '책 제목 1',
+    author: '홍길동',
+  },
+  {
+    id: '2',
+    title: '책 제목 2',
+    author: '홍길동',
+  },
+  {
+    id: '3',
+    title: '책 제목 3',
+
+    author: '홍길동',
+  },
+  {
+    id: '4',
+    title: '책 제목 4',
+    author: '홍길동',
+  },
+  {
+    id: '5',
+    title: '책 제목 5',
+    author: '홍길동',
+  },
+];
+
+export const getBooks = () => {
+  return mockBooks;
+};
