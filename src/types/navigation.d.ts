c3aa71c48188b13532f65c1ef334663973320a28
jiu <jiu@jiuui-MacBookPro.local>
2025-04-04 15:04:31 +0900

    chore: tab navigate 중첩 사용을 위한 타입 선언

--- src/types/navigation.d.ts
+++ src/types/navigation.d.ts
@@ -2,11 +2,13 @@ import {NativeStackScreenProps} from '@react-navigation/native-stack';
 
 declare global {
   type RootStackParamList = {
+    MainTabs: undefined;
     Home: undefined;
     MyBook: undefined;
     Library: undefined;
     MyPage: undefined;
     CreateBook: undefined;
+    MainTabs: undefined;
   };
 
   type RootStackScreenNames = keyof RootStackParamList;
