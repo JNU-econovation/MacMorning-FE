5894718013b62fb487ea220356a93383bdd8d0f7
jiu <jiu@jiuui-MacBookPro.local>
2025-04-04 14:00:44 +0900

    feat: 라우팅을 위한 임시 페이지 생성

---
+++ src/pages/createBook/CreateBook.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+function CreateBook(): React.JSX.Element {
+  return (
+    <View>
+      <Text>CreateBook</Text>
+    </View>
+  );
+}
+
+export default CreateBook;
