5403ac09845b87d3f8453dfcbbdd99ff52fcff0b
jiucchu <jiucchu@gmail.com>
2025-04-05 18:36:07 +0900

    refactor: 절대 경로 설정을 위한 의존성 변경

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,27 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
-  plugins: ['nativewind/babel', 'react-native-reanimated/plugin'],
+  plugins: [
+    'nativewind/babel',
+    'react-native-reanimated/plugin',
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: [
+          '.ios.ts',
+          '.android.ts',
+          '.ts',
+          '.ios.tsx',
+          '.android.tsx',
+          '.tsx',
+          '.jsx',
+          '.js',
+          '.json',
+        ],
+        alias: {
+          '@': './src',
+        },
+      },
+    ],
+  ],
 };
