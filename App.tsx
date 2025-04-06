753c48e512216e345580cc4433681ea0f57f312c
jiucchu <jiucchu@gmail.com>
2025-04-06 19:11:01 +0900

    refactor: 불필요한 import 삭제

--- App.tsx
+++ App.tsx
@@ -4,12 +4,8 @@ import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import 'react-native-reanimated';
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
-import Home from './src/pages/home/Home';
-import BottomBar from './src/components/common/bottomBar/BottomBar';
 import CreateBook from './src/pages/createBook/CreateBook';
-import Library from './src/pages/library/Library';
-import MyBook from './src/pages/myBook/Mybook';
-import MyPage from './src/pages/myPage/MyPage';
+
 import BottomTabNavigator from './src/utils/navigate/BottomTabNavigator';
 
 const Stack = createStackNavigator<RootStackParamList>();
