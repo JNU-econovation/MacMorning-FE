1c7c53140137c15aa376c4672aae69998696d0db
jiucchu <jiucchu@gmail.com>
2025-05-07 10:44:41 +0900

    feat: 캐릭터 인풋 수정

--- src/components/createBook/characterSelect/CharacterSelectInput.tsx
+++ src/components/createBook/characterSelect/CharacterSelectInput.tsx
@@ -1,38 +1,36 @@
 import React from 'react';
-import {TextInput, View} from 'react-native';
-import CustomText from '@/utils/CustomText';
+import {TextInput, View, StyleProp, ViewStyle} from 'react-native';
 import styled from 'styled-components/native';
 import {COLORS} from '@/constants/colors';
 import {scale} from 'react-native-size-matters';
 
 type Props = {
-  label: string;
   value: string;
   onChange: (value: string) => void;
+  style?: StyleProp<ViewStyle>;
 };
 
-const CharacterSelectInput = ({label, value, onChange}: Props) => {
+const CharacterSelectInput = ({value, onChange, style}: Props) => {
   return (
-    <CharacterSelectInputContainer>
-      <CustomText>{label}</CustomText>
+    <CharacterSelectInputContainer style={style}>
       <CharacterSelectInputInput value={value} onChangeText={onChange} />
     </CharacterSelectInputContainer>
   );
 };
 
 const CharacterSelectInputContainer = styled.View`
-  width: 20%;
   flex-direction: row;
   align-items: center;
   justify-content: space-between;
 `;
 
 const CharacterSelectInputInput = styled.TextInput`
-  width: ${scale(100)}px;
-  height: ${scale(20)}px;
-  background-color: ${COLORS.background.lightGray};
+  width: 100%;
+  height: 100%;
   border-radius: ${scale(10)}px;
-  padding: ${scale(1)}px;
+  border-width: 2px;
+  border-color: ${COLORS.background.lightGray};
+  padding: ${scale(5)}px;
 `;
 
 export default CharacterSelectInput;
