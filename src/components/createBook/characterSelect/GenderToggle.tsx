8cc08ce28072c9d2d78c0aed8e9eb8c6cf0065bd
jiucchu <jiucchu@gmail.com>
2025-05-07 10:44:06 +0900

    feat: gender toggle 구현

--- src/components/createBook/characterSelect/GenderToggle.tsx
+++ src/components/createBook/characterSelect/GenderToggle.tsx
@@ -8,10 +8,9 @@ import CustomText from '@/utils/CustomText';
 type Props = {
   onToggle: () => void;
   isOn: boolean;
-  label: string;
 };
 
-const GenderToggle = ({onToggle, isOn, label}: Props) => {
+const GenderToggle = ({onToggle, isOn}: Props) => {
   const [animatedValue] = useState(new Animated.Value(isOn ? 1 : 0));
 
   useEffect(() => {
@@ -32,7 +31,6 @@ const GenderToggle = ({onToggle, isOn, label}: Props) => {
 
   return (
     <GenderToggleContainer>
-      <CustomText>{label}</CustomText>
       <ToggleContainer activeOpacity={1} onPress={onToggle} color={color}>
         <ToggleTextContainer style={{left: 0}}>
           <CustomText font="NPSfont_regular">남자</CustomText>
@@ -55,6 +53,7 @@ export default GenderToggle;
 const GenderToggleContainer = styled.View`
   flex-direction: row;
   align-items: center;
+  gap: ${scale(5)}px;
 `;
 
 const ToggleContainer = styled.TouchableOpacity<{color: string}>`
