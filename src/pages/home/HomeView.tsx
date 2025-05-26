88fa03348ab0d2c40d472a8a4ca6a46b9b7a80cb
jiucchu <jiucchu@gmail.com>
2025-05-26 10:47:59 +0900

    refactor: 메인페이지 수정

--- src/pages/home/HomeView.tsx
+++ src/pages/home/HomeView.tsx
@@ -1,16 +1,33 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {View, FlatList, ListRenderItem} from 'react-native';
 import styled from 'styled-components/native';
 import BookComponent from '@/components/common/book/BookComponent';
-import {getBooks} from '@/apis/book/mock';
+import {getRecentMyBooks, getMainBestBooks} from '@/apis/book/getBooks';
 import {scale} from 'react-native-size-matters';
 import {COLORS} from '@/constants/colors';
 import TextNavigateButton from '@/components/home/TextNavigateButton';
 import {useNavigation} from '@react-navigation/native';
 
 const HomeView = (): React.JSX.Element => {
-  const books: Book[] = getBooks();
-  console.log(books);
+  const [MyBooks, setMyBooks] = useState<Book[]>([]);
+  const [nextMyBookCursor, setNextMyBookCursor] = useState<string | null>(null);
+  const [bestBooks, setBestBooks] = useState<Book[]>([]);
+  const [nextBestCursor, setNextBestCursor] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchBooks = async () => {
+      const {books: myBooks, nextCursor: myBooksCursor} =
+        await getRecentMyBooks();
+      setMyBooks(myBooks);
+      setNextMyBookCursor(myBooksCursor);
+
+      const {books: bestBooks, nextCursor: bestBooksCursor} =
+        await getMainBestBooks();
+      setBestBooks(bestBooks);
+      setNextBestCursor(bestBooksCursor);
+    };
+    fetchBooks();
+  }, []);
 
   const renderItem: ListRenderItem<Book> = ({item: book}) => (
     <BookComponent book={book} />
@@ -31,7 +48,30 @@ const HomeView = (): React.JSX.Element => {
         />
       </HomeButtonContainer>
       <FlatList<Book>
-        data={books}
+        data={MyBooks}
+        renderItem={renderItem}
+        keyExtractor={book => book.id}
+        numColumns={4}
+        scrollEnabled={false}
+        columnWrapperStyle={{
+          marginBottom: scale(20),
+          gap: '8%',
+        }}
+      />
+
+      <HomeButtonContainer>
+        <TextNavigateButton
+          onPress={() => navigation.navigate('MyBook')}
+          text="Best 이야기"
+          fontInfo={{
+            font: 'NPSfont_bold',
+            fontSize: scale(12),
+            fontColor: COLORS.text.primary,
+          }}
+        />
+      </HomeButtonContainer>
+      <FlatList<Book>
+        data={bestBooks}
         renderItem={renderItem}
         keyExtractor={book => book.id}
         numColumns={4}
