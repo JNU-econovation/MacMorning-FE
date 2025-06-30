e3e80fa5fce9d0c2c2810a6d798201d5ddd12822
jiucchu <jiucchu@gmail.com>
2025-06-30 10:53:16 +0900

    refactor: 캐릭터 정보 입력 시 공통 타입 사용

--- src/pages/createBook/selectView/CharacterSelectView.tsx
+++ src/pages/createBook/selectView/CharacterSelectView.tsx
@@ -7,22 +7,7 @@ import Toggle from '@/components/common/toggle/Toggle';
 import DropDownToggle from '@/components/common/toggle/DropDownToggle';
 import CharacterSelectInput from '@/components/createBook/characterSelect/CharacterSelectInput';
 import GenderToggle from '@/components/createBook/characterSelect/GenderToggle';
-
-interface FormData {
-  genre: string[];
-  gameMode: boolean;
-  story: {
-    perspective: string;
-    background: string;
-  };
-  character: {
-    name: string;
-    gender: string;
-    age: string;
-    description: string;
-  };
-}
-
+import {FormData} from '@/types/form';
 type CharacterSelectViewProps = {
   initialData: FormData;
   setFormData: React.Dispatch<React.SetStateAction<FormData>>;
@@ -97,7 +82,6 @@ const CharacterSelectView = (
 
 const CharacterSelectContainer = styled.View`
   gap: ${scale(10)}px;
-  height: 100%;
   width: 100%;
 `;
 
