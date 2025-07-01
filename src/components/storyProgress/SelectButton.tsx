8c70e6912780645c436ed6b8eb8b699d0d7a3b2d
jiucchu <jiucchu@gmail.com>
2025-07-01 10:24:37 +0900

    style: 선택 버튼 스타일 구현

---
+++ src/components/storyProgress/SelectButton.tsx
@@ -0,0 +1,44 @@
+import React, {useState} from 'react';
+import styled from 'styled-components/native';
+import CustomText from '@/utils/CustomText';
+import {scale} from 'react-native-size-matters';
+import {COLORS} from '@/constants/colors';
+
+const SelectButton = ({
+  onPress,
+  selectScript,
+}: {
+  onPress: () => void;
+  selectScript: string;
+}) => {
+  const [isPressed, setIsPressed] = useState(false);
+
+  return (
+    <ButtonContainer
+      onPress={onPress}
+      onPressIn={() => setIsPressed(true)}
+      onPressOut={() => setIsPressed(false)}
+      isPressed={isPressed}>
+      <ButtonText font="NPSfont_regular">{selectScript}</ButtonText>
+    </ButtonContainer>
+  );
+};
+
+const ButtonContainer = styled.Pressable<{isPressed: boolean}>`
+  width: 80%;
+  height: ${scale(22)}px;
+  background-color: ${COLORS.background.white};
+  border-radius: ${scale(10)}px;
+  justify-content: center;
+  align-items: center;
+  border: 2px solid
+    ${({isPressed}: {isPressed: boolean}) =>
+      isPressed ? COLORS.primary : COLORS.text.secondary};
+`;
+
+const ButtonText = styled(CustomText)`
+  font-size: ${scale(8)}px;
+  color: #000;
+`;
+
+export default SelectButton;
