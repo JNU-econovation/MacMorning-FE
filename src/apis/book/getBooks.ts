d192ef968622da11ac8f2047efe20f0cca233226
jiucchu <jiucchu@gmail.com>
2025-07-13 17:31:04 +0900

    bug: 메인 화면 도서 조회 불가 에러 해결

--- src/apis/book/getBooks.ts
+++ src/apis/book/getBooks.ts
@@ -15,29 +15,32 @@ async function getBooks(cursor?: string) {
   };
 }
 
-async function getRecentMyBooks(cursor?: string, orderStrategy?: string) {
+async function getRecentMyBooks(orderStrategy?: string, accessToken?: string) {
   const baseUrl = 'https://api.ilovejokbal.monster/v1/books/mybooks';
   const limit = 4;
 
-  const url = cursor
-    ? `${baseUrl}?limit=${limit}&order_strategy=${orderStrategy}&cursor=${cursor}`
-    : `${baseUrl}?limit=${limit}&order_strategy=${orderStrategy}`;
+  const url = `${baseUrl}?limit=${limit}&order_strategy=${orderStrategy}`;
 
-  const response = await (await fetch(url)).json();
-
+  console.log('url', url);
+  console.log('accessToken', accessToken);
+  const response = await (
+    await fetch(url, {
+      headers: {
+        Authorization: `${accessToken}`,
+      },
+    })
+  ).json();
+  console.log('response', response);
   return {
     books: response.data.books,
     nextCursor: response.data.next_cursor,
   };
 }
 
-async function getMainBestBooks(cursor?: string) {
+async function getMainBestBooks() {
   const baseUrl = 'https://api.ilovejokbal.monster/v1/books/best';
   const limit = 4;
-
-  const url = cursor
-    ? `${baseUrl}?limit=${limit}&cursor=${cursor}`
-    : `${baseUrl}?limit=${limit}`;
+  const url = `${baseUrl}?limit=${limit}`;
 
   const response = await (await fetch(url)).json();
 
