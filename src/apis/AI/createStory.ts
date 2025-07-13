01a9287a203cc7a37ddf15baca88456dabc058b3
jiucchu <jiucchu@gmail.com>
2025-07-13 21:57:53 +0900

    const: AI base url 설정

--- src/apis/AI/createStory.ts
+++ src/apis/AI/createStory.ts
@@ -1,17 +1,15 @@
-import {baseUrl} from '@/constants/api';
+import {AI_BASE_URL} from '@/constants/api';
 import {FormData} from '@/types/form';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-export const createStory = async (data: FormData) => {
+export const createStory = async (data: FormData, bookId: number) => {
   const accessToken = await AsyncStorage.getItem('accessToken');
-  const baseUrl = 'https://api.ilovejokbal.monster/v1';
-  const response = await fetch(`${baseUrl}/book/story/start`, {
+  const response = await fetch(`${AI_BASE_URL}/book/${bookId}/story/start`, {
     method: 'POST',
     body: JSON.stringify({
       title: data.title,
       genre: data.genre,
-      gamemode: data.gameMode,
-      chracter: {
+      character: {
         grammatical_person: data.story.grammatical_person,
         historical_background: data.story.historical_background,
         name: data.character.name,
--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,1 +1,2 @@
 export const baseUrl = 'https://api.ilovejokbal.monster/v1';
+export const AI_BASE_URL = 'https://ai.ilovejokbal.monster/v1';
