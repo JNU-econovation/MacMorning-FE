9403c374600cfe1a1d3cc1b6eb16184d5d09ca9a
jiucchu <jiucchu@gmail.com>
2025-07-14 05:40:38 +0900

    feat: 내 책 불러오기 구현

--- src/apis/book/getBooks.ts
+++ src/apis/book/getBooks.ts
@@ -14,6 +14,33 @@ async function getBooks(cursor?: string) {
     nextCursor: response.data.next_cursor,
   };
 }
+async function getMyBooks(
+  orderStrategy?: string,
+  accessToken?: string,
+  cursor?: string,
+) {
+  const baseUrl = 'https://api.ilovejokbal.monster/v1/books/mybooks';
+  const limit = 8;
+
+  let url = `${baseUrl}?limit=${limit}&order_strategy=${orderStrategy}`;
+  if (cursor) {
+    url += `&cursor=${cursor}`;
+  }
+
+  const response = await (
+    await fetch(url, {
+      headers: {
+        Authorization: `${accessToken}`,
+      },
+    })
+  ).json();
+
+  console.log('response', response);
+  return {
+    books: response.data.books,
+    nextCursor: response.data.next_cursor,
+  };
+}
 
 async function getRecentMyBooks(orderStrategy?: string, accessToken?: string) {
   const baseUrl = 'https://api.ilovejokbal.monster/v1/books/mybooks';
@@ -40,7 +67,6 @@ async function getMainBestBooks() {
   const limit = 4;
   const url = `${baseUrl}?limit=${limit}`;
 
-
   const response = await (await fetch(url)).json();
 
   return {
@@ -49,4 +75,4 @@ async function getMainBestBooks() {
   };
 }
 
-export {getBooks, getRecentMyBooks, getMainBestBooks};
+export {getBooks, getMyBooks, getRecentMyBooks, getMainBestBooks};
