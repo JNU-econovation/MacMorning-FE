e6df90398558850b049ea446b8a8bd3c4e412937
jiucchu <jiucchu@gmail.com>
2025-07-16 05:26:45 +0900

    feat: 저장된 이야기 progress 페이지 이동을 위한 헬퍼 함수 수정

--- src/utils/navigate/NavigateHelpers.tsx
+++ src/utils/navigate/NavigateHelpers.tsx
@@ -1,10 +1,14 @@
 import {NavigationProp} from '@react-navigation/native';
+import {FormData} from '@/types/form';
 
 export const createNavigationHelpers = (
   navigation: NavigationProp<RootStackParamList>,
 ) => ({
   goToStoryProgress: (props: {
     bookId: number;
-    AIResponse: {newStory: string};
+    formData: FormData;
+    lastPage?: number;
   }) => navigation.navigate('StoryProgress', {props}),
+  goToCreatedBookProgress: (props: {bookId: number; lastPage: number}) =>
+    navigation.navigate('StoryProgress', {props}),
 });
