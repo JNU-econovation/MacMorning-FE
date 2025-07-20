c1f7d12c43dc53a5a1b3ecde73af9e4e23ad6935
jiucchu <jiucchu@gmail.com>
2025-07-21 08:16:04 +0900

    feat: 이메일 및 비밀번호 검증 구현

---
+++ src/utils/validator/validator.tsx
@@ -0,0 +1,11 @@
+const validateEmail = (email: string) => {
+  const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return regex.test(email);
+};
+
+const validatePassword = (password: string) => {
+  const regex = /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+  return regex.test(password);
+};
+
+export {validateEmail, validatePassword};
