2cb6967a7c43d76f30a84a829581f54b1aa30159
jiucchu <jiucchu@gmail.com>
2025-07-22 20:24:19 +0900

    feat: 썸네일 선택

--- src/pages/ChooseThumbnail/ChooseThumbnail.tsx
+++ src/pages/ChooseThumbnail/ChooseThumbnail.tsx
@@ -5,21 +5,25 @@ import {scale} from 'react-native-size-matters';
 import ChooseThumbnailView from './ChooseThumbnailView';
 import StepButton from '@/components/common/buttons/StepButton';
 import {getBookDetail} from '@/apis/book/getBook';
-import { getBookAllIllust, setThumbnail } from '@/apis/Illust/illusts';
+import {getBookAllIllust, setThumbnail} from '@/apis/Illust/illusts';
 import {useAuthStore} from '@/store/authStore';
 import {BookDetail} from '@/types/book';
-import { Illust } from '@/types/book';
+import {Illust} from '@/types/book';
 import {Alert} from 'react-native';
+import {CommonActions, useNavigation} from '@react-navigation/native';
 
 type ChooseThumbnailProps = RootStackScreenProps<'ChooseThumbnail'>;
 
-const ChooseThumbnail = ({ route }: ChooseThumbnailProps): React.JSX.Element => {
+const ChooseThumbnail = ({route}: ChooseThumbnailProps): React.JSX.Element => {
   const {bookId} = route.params?.props || {};
   const accessToken = useAuthStore(state => state.accessToken);
-  
+  const navigation = useNavigation<RootStackNavigationProp>();
+
   const [bookDetail, setBookDetail] = useState<BookDetail | null>(null);
   const [illust, setIllust] = useState<Illust[]>([]);
-  const [selectedThumbnailId, setSelectedThumbnailId] = useState<number | null>(null);
+  const [selectedThumbnailId, setSelectedThumbnailId] = useState<number | null>(
+    null,
+  );
 
   useEffect(() => {
     const fetchData = async () => {
@@ -33,8 +37,9 @@ const ChooseThumbnail = ({ route }: ChooseThumbnailProps): React.JSX.Element => 
         // 2. 모든 일러스트 불러오기
         const illustResponse = await getBookAllIllust(bookId, accessToken);
         if (illustResponse) {
-          setIllust(illustResponse);
+          setIllust(illustResponse.filter(item => item.image_url !== ''));
         }
+        console.log('illustResponse', illust);
       } catch (error) {
         console.error('ChooseThumbnail 데이터 fetch 에러:', error);
       }
@@ -43,28 +48,49 @@ const ChooseThumbnail = ({ route }: ChooseThumbnailProps): React.JSX.Element => 
   }, [bookId, accessToken]);
 
   const handleSelectThumbnail = (selectedId: number | null) => {
-      setSelectedThumbnailId(selectedId);
+    setSelectedThumbnailId(selectedId);
   };
 
   const handleChoose = async () => {
+    if (illust.length === 0) {
+      navigation.dispatch(
+        CommonActions.reset({
+          index: 0,
+          routes: [{name: 'MainTabs', params: {screen: 'Home'}}],
+        }),
+      );
+      return;
+    }
+
     if (!selectedThumbnailId) {
       Alert.alert('알림', '썸네일을 선택해주세요.');
       return;
     }
 
     // 선택된 ID로 해당 일러스트 찾기
-    const selectedIllust = illust.find(item => item.illust_id === selectedThumbnailId);
-    
+    const selectedIllust = illust.find(
+      item => item.illust_id === selectedThumbnailId,
+    );
+
     if (!selectedIllust || !selectedIllust.image_url) {
       Alert.alert('오류', '선택된 이미지를 찾을 수 없습니다.');
       return;
     }
 
     try {
-      const success = await setThumbnail(bookId, selectedIllust.image_url, accessToken);
-      
+      const success = await setThumbnail(
+        bookId,
+        selectedIllust.image_url,
+        accessToken,
+      );
+
       if (success) {
-        //navigate~
+        navigation.dispatch(
+          CommonActions.reset({
+            index: 0,
+            routes: [{name: 'MainTabs', params: {screen: 'Home'}}],
+          }),
+        );
       } else {
         Alert.alert('오류', '썸네일 설정에 실패했습니다.');
       }
@@ -74,21 +100,18 @@ const ChooseThumbnail = ({ route }: ChooseThumbnailProps): React.JSX.Element => 
     }
   };
 
-
   return (
     <ChooseThumbnailContainer>
       <Header title={bookDetail?.title || '썸네일 선택'} headerType="create" />
       <ChooseThumbnailViewContainer>
-        <ChooseThumbnailView 
+        <ChooseThumbnailView
           illust={illust}
-          onSelectThumbnail={handleSelectThumbnail}/>
+          onSelectThumbnail={handleSelectThumbnail}
+        />
       </ChooseThumbnailViewContainer>
       <ProgressButtonContainer>
         <ProgressButtonWrapper>
-          <StepButton
-            text="선택 완료"
-            onPress={handleChoose}
-          />
+          <StepButton text="선택 완료" onPress={handleChoose} />
         </ProgressButtonWrapper>
       </ProgressButtonContainer>
     </ChooseThumbnailContainer>
@@ -110,7 +133,7 @@ const ChooseThumbnailViewContainer = styled.View`
 
 const ProgressButtonContainer = styled.View`
   width: 100%;
-  align-items: center
+  align-items: center;
 `;
 
 const ProgressButtonWrapper = styled.View`
--- src/pages/ChooseThumbnail/ChooseThumbnailView.tsx
+++ src/pages/ChooseThumbnail/ChooseThumbnailView.tsx
@@ -4,120 +4,133 @@ import styled from 'styled-components/native';
 import {scale} from 'react-native-size-matters';
 import {COLORS} from '@/constants/colors';
 import CustomText from '@/utils/CustomText';
-import { Illust } from '@/types/book';
+import {Illust} from '@/types/book';
 import {cloudFrontDomain} from '@/constants/domain';
 
 interface ChooseThumbnailViewProps {
-    illust: Illust[];
-    onSelectThumbnail: (selectedId: number | null) => void;
+  illust: Illust[];
+  onSelectThumbnail: (selectedId: number | null) => void;
 }
 
-const ChooseThumbnailView = ({illust, onSelectThumbnail}: ChooseThumbnailViewProps) => {
-    const [selectedId, setSelectedId] = useState<number | null>(null);
+const ChooseThumbnailView = ({
+  illust,
+  onSelectThumbnail,
+}: ChooseThumbnailViewProps) => {
+  const [selectedId, setSelectedId] = useState<number | null>(null);
 
-    // illust가 배열로 받아와져서 hook이 안먹혀서 만든 함수
-    const getImageUrl = (imageUrl: string | null | undefined): string => {
-        if (!imageUrl) {
-            return `${cloudFrontDomain}/basic.png`;
-        }
-        return `${cloudFrontDomain}/${imageUrl}`;
-    };
+  // illust가 배열로 받아와져서 hook이 안먹혀서 만든 함수
+  const getImageUrl = (imageUrl: string | null | undefined): string => {
+    if (!imageUrl) {
+      return `${cloudFrontDomain}/basic.png`;
+    }
+    return `${cloudFrontDomain}/${imageUrl}`;
+  };
 
-
-    const renderThumbnailItem = ({item}: {item: Illust}) => {
-        const imageUrl = getImageUrl(item.image_url);
-        const isSelected = selectedId === item.illust_id;
-
-        return (
-            <ThumbnailOption 
-            isSelected={isSelected}
-            onPress={() => handleSelectThumbnail(item.illust_id)}
-            >
-                {imageUrl ? (
-                    <ThumbnailImage source={{uri: imageUrl}} />
-                ) : (
-                    <ThumbnailPlaceholder />
-                )}
-            </ThumbnailOption>
-        );
-    };
-
-    const handleSelectThumbnail = (illustId: number) => {
-        setSelectedId(illustId);
-        onSelectThumbnail(illustId);
-    };
+  const renderThumbnailItem = ({item}: {item: Illust}) => {
+    const imageUrl = getImageUrl(item.image_url);
+    const isSelected = selectedId === item.illust_id;
 
     return (
-        <ChooseThumbnailViewContainer>
-            <ChooseTextContainer>
-                <CustomText font="NPSfont_bold" style={{fontSize: scale(10)}}>
-                    이야기를 대표할 이미지를 선택해주세요.
-                </CustomText>
-            </ChooseTextContainer>
-            <ThumbnailFlatListContainer>
-                <FlatList
-                    data={illust}
-                    renderItem={renderThumbnailItem}
-                    keyExtractor={(item) => item.illust_id.toString()}
-                    numColumns={4}
-                    showsVerticalScrollIndicator={false}
-                    scrollEnabled={true}
-                    contentContainerStyle={{
-                        paddingTop: scale(20),
-                        paddingBottom: scale(20),
-                    }}
-                    columnWrapperStyle={{
-                        width: '100%',
-                        marginBottom: scale(15),
-                        gap:'4%',
-                    }}
-                />
-            </ThumbnailFlatListContainer>
-        </ChooseThumbnailViewContainer>
+      <ThumbnailOption
+        isSelected={isSelected}
+        onPress={() => handleSelectThumbnail(item.illust_id)}>
+        {imageUrl ? (
+          <ThumbnailImage source={{uri: imageUrl}} />
+        ) : (
+          <ThumbnailPlaceholder />
+        )}
+      </ThumbnailOption>
     );
+  };
+
+  const handleSelectThumbnail = (illustId: number) => {
+    setSelectedId(illustId);
+    onSelectThumbnail(illustId);
+  };
+
+  return (
+    <ChooseThumbnailViewContainer>
+      <ChooseTextContainer>
+        <CustomText font="NPSfont_bold" style={{fontSize: scale(10)}}>
+          이야기를 대표할 이미지를 선택해주세요.
+        </CustomText>
+      </ChooseTextContainer>
+      <ThumbnailFlatListContainer>
+        {illust.length > 0 ? (
+          <FlatList
+            data={illust}
+            renderItem={renderThumbnailItem}
+            keyExtractor={item => item.illust_id.toString()}
+            numColumns={4}
+            showsVerticalScrollIndicator={false}
+            scrollEnabled={true}
+            contentContainerStyle={{
+              paddingTop: scale(20),
+              paddingBottom: scale(20),
+            }}
+            columnWrapperStyle={{
+              width: '100%',
+              marginBottom: scale(15),
+              gap: '4%',
+            }}
+          />
+        ) : (
+          <CustomText
+            font="NPSfont_regular"
+            style={{fontSize: scale(10), color: COLORS.text.secondary}}>
+            썸네일이 없습니다.
+          </CustomText>
+        )}
+      </ThumbnailFlatListContainer>
+    </ChooseThumbnailViewContainer>
+  );
 };
 
 const ChooseThumbnailViewContainer = styled.View`
-    background-color: ${COLORS.background.white};
-    width: 90%;
-    height: 100%;
-    border-radius: ${scale(10)}px;
-    padding-vertical : ${scale(15)}px;
-    padding-horizontal : 5%;
+  background-color: ${COLORS.background.white};
+  width: 90%;
+  height: 100%;
+  border-radius: ${scale(10)}px;
+  padding-vertical: ${scale(15)}px;
+  padding-horizontal: 5%;
 `;
 
 const ChooseTextContainer = styled.View`
-    width: 100%;
-    align-items : center;
-    padding-top : ${scale(15)}px;
-    padding-bottom : ${scale(15)}px;
+  width: 100%;
+  align-items: center;
+  padding-top: ${scale(15)}px;
+  padding-bottom: ${scale(15)}px;
 `;
 
 const ThumbnailFlatListContainer = styled.View`
-    flex: 1;
-    width: 100%;
+  flex: 1;
+  width: 100%;
+  justify-content: center;
+  align-items: center;
 `;
 
 const ThumbnailOption = styled.TouchableOpacity`
-    width: 22%;
-    aspect-ratio: 0.8;
-    border-radius: ${scale(8)}px;
-    border-width: ${(props: {isSelected: boolean}) => props.isSelected ? scale(2) : scale(1)}px;
-    border-color: ${(props: {isSelected: boolean}) => props.isSelected ? COLORS.primary : COLORS.background.lightGray};
-    background-color: ${COLORS.background.lightGray};
+  width: 22%;
+  aspect-ratio: 0.8;
+  border-radius: ${scale(8)}px;
+  border-width: ${(props: {isSelected: boolean}) =>
+    props.isSelected ? scale(2) : scale(1)}px;
+  border-color: ${(props: {isSelected: boolean}) =>
+    props.isSelected ? COLORS.primary : COLORS.background.lightGray};
+  background-color: ${COLORS.background.lightGray};
 `;
 
 const ThumbnailPlaceholder = styled.View`
-    flex: 1;
-    border-radius: ${scale(6)}px;
-    background-color: ${COLORS.background.lightGray};
+  flex: 1;
+  border-radius: ${scale(6)}px;
+  background-color: ${COLORS.background.lightGray};
 `;
 
 const ThumbnailImage = styled.Image`
-    flex: 1;
-    width: 100%;
-    height: 100%;
-    border-radius: ${scale(6)}px;
+  flex: 1;
+  width: 100%;
+  height: 100%;
+  border-radius: ${scale(6)}px;
 `;
 
 export default ChooseThumbnailView;
