3507b050f4d9c7b95520e9b1ea82d69927366888
jiucchu <jiucchu@gmail.com>
2025-07-24 19:23:38 +0900

    feat: 선택된 카테고리의 색상 변경

--- src/components/common/bottomBar/BottomBar.tsx
+++ src/components/common/bottomBar/BottomBar.tsx
@@ -2,21 +2,55 @@ import React from 'react';
 import styled from 'styled-components/native';
 import {scale} from 'react-native-size-matters';
 import {COLORS} from '@/constants/colors';
-import BottomBarButton from './BottomBarButton';
 import BottomBarPlusButton from './BottomBarPlusButton';
 import HomeSVG from '@/assets/images/bottomBar/home.svg';
 import BookSVG from '@/assets/images/bottomBar/book.svg';
 import OpenBookSVG from '@/assets/images/bottomBar/book-open.svg';
 import UserSVG from '@/assets/images/bottomBar/user.svg';
 import {BottomTabBarProps} from '@react-navigation/bottom-tabs';
-import { Platform } from 'react-native';
+import {Platform, Text} from 'react-native';
+import CustomText from '@/utils/CustomText';
 
-const BottomBar = ({navigation}: BottomTabBarProps): React.JSX.Element => {
+interface BottomBarButtonProps {
+  icon: React.ReactNode;
+  buttonName: string;
+  onPress: () => void;
+  isActive: boolean;
+}
+
+const BottomBarButton = ({
+  icon,
+  buttonName,
+  onPress,
+  isActive,
+}: BottomBarButtonProps) => {
+  return (
+    <BottomBarButtonWrapper onPress={onPress}>
+      <Text style={{justifyContent: 'center', alignItems: 'center'}}>
+        {icon}
+      </Text>
+      <CustomText
+        font="NPSfont_regular"
+        style={{
+          fontSize: scale(7),
+          color: isActive ? COLORS.primary : COLORS.text.primary,
+        }}>
+        {buttonName}
+      </CustomText>
+    </BottomBarButtonWrapper>
+  );
+};
+
+const BottomBar = ({
+  navigation,
+  state,
+}: BottomTabBarProps): React.JSX.Element => {
   const iconProps: IconProps = {
     width: scale(15),
     height: scale(15),
     strokeWidth: scale(0.8),
   };
+  const currentRoute = state.routes[state.index].name;
 
   return (
     <>
@@ -27,26 +61,64 @@ const BottomBar = ({navigation}: BottomTabBarProps): React.JSX.Element => {
       <BottomBarContainer>
         <BottomBarButtonContainer>
           <BottomBarButton
-            icon={<HomeSVG {...iconProps} />}
+            icon={
+              <HomeSVG
+                color={
+                  currentRoute === 'Home' ? COLORS.primary : COLORS.text.primary
+                }
+                {...iconProps}
+              />
+            }
             buttonName="홈"
             onPress={() => navigation.navigate('Home')}
+            isActive={currentRoute === 'Home'}
           />
           <BottomBarButton
-            icon={<OpenBookSVG {...iconProps} />}
+            icon={
+              <OpenBookSVG
+                color={
+                  currentRoute === 'MyBook'
+                    ? COLORS.primary
+                    : COLORS.text.primary
+                }
+                {...iconProps}
+              />
+            }
             buttonName="내 책"
             onPress={() => navigation.navigate('MyBook')}
+            isActive={currentRoute === 'MyBook'}
           />
         </BottomBarButtonContainer>
         <BottomBarButtonContainer>
           <BottomBarButton
-            icon={<BookSVG {...iconProps} />}
+            icon={
+              <BookSVG
+                color={
+                  currentRoute === 'Library'
+                    ? COLORS.primary
+                    : COLORS.text.primary
+                }
+                {...iconProps}
+              />
+            }
             buttonName="이야기 도서관"
             onPress={() => navigation.navigate('Library')}
+            isActive={currentRoute === 'Library'}
           />
           <BottomBarButton
-            icon={<UserSVG {...iconProps} />}
+            icon={
+              <UserSVG
+                color={
+                  currentRoute === 'MyPage'
+                    ? COLORS.primary
+                    : COLORS.text.primary
+                }
+                {...iconProps}
+              />
+            }
             buttonName="마이페이지"
             onPress={() => navigation.navigate('MyPage')}
+            isActive={currentRoute === 'MyPage'}
           />
         </BottomBarButtonContainer>
       </BottomBarContainer>
@@ -74,6 +146,13 @@ const BottomBarButtonContainer = styled.View`
   justify-content: center;
   align-items: center;
   gap: 30%;
+`;
+
+const BottomBarButtonWrapper = styled.TouchableOpacity`
+  justify-content: center;
+  align-items: center;
+  gap: ${scale(2)}px;
+  width: 25%;
 `;
 
 export default BottomBar;
